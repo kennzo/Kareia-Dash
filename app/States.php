3ad3bf5e609be3e85d5941bf599159a1756cf26f
Kenneth Sok <kenneth.sok@gmail.com>
2017-04-18 05:03:52 +0000

    (#10) Add States entity
    Added the states entity with the migration.
    
    Added the seeding file which populates with all the states and others.

---
+++ app/States.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class States extends Model
+{
+    // todo: Find out about presenter for this
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id', 'name', 'abbreviation'
+    ];
+
+    // Sets the usage of timestamps to false. Need this so that when you run the seeder, you don't get an error.
+    public $timestamps = false;
+}
---
+++ database/migrations/2017_04_18_043138_create_states_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('states', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('abbreviation');
+        });
+
+        // todo: Create foreign key for properties to states
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('states');
+    }
+}
