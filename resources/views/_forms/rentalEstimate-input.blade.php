2b2e633abcd61409306dc608be72fc6baf3800c5
Kenneth Sok <kenneth.sok@gmail.com>
2017-10-15 05:09:09 +0000

    (#35) Shrink size of inputs
    Apply new styles.

--- resources/views/_forms/rentalEstimate-input.blade.php
+++ resources/views/_forms/rentalEstimate-input.blade.php
@@ -23,80 +23,84 @@
 
     <div class="well">
         <h4>Purchase and Finance</h4>
-        <div class='form-group'>
-            {!! Form::label('arv', 'After Repair Value (ARV)', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'arv',
-                    isset($rentalEstimate)? $rentalEstimate->arv : null,
-                    ['class' => 'form-control']) !!}
-            </div>
-        </div>
+        <div class="row">
+            <div class="col-md-4">
+                <div class='form-group'>
+                    {!! Form::label('arv', 'After Repair Value (ARV)', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'arv',
+                            isset($rentalEstimate)? $rentalEstimate->arv : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
 
-        <div class='form-group'>
-            {!! Form::label('purchase_price', 'Purchase Price *', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'purchase_price',
-                    isset($rentalEstimate)? $rentalEstimate->purchase_price : null,
-                    ['class' => 'form-control']) !!}
-            </div>
-        </div>
+                <div class='form-group'>
+                    {!! Form::label('purchase_price', 'Purchase Price *', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'purchase_price',
+                            isset($rentalEstimate)? $rentalEstimate->purchase_price : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
 
-        <div class='form-group'>
-            {!! Form::label('repairs', 'Repairs:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'repairs',
-                    isset($rentalEstimate)? $rentalEstimate->repairs : null,
-                    ['class' => 'form-control']) !!}
-            </div>
-        </div>
+                <div class='form-group'>
+                    {!! Form::label('repairs', 'Repairs:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'repairs',
+                            isset($rentalEstimate)? $rentalEstimate->repairs : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
 
-        <div class='form-group'>
-            {!! Form::label('financed', 'Financed:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8">
-                {!! Form::checkbox(
-                    'financed',
-                    1,
-                    isset($rentalEstimate)? $rentalEstimate->financed : null,
-                    ['class' => 'form-control']) !!}
-            </div>
-        </div>
+                <div class='form-group'>
+                    {!! Form::label('financed', 'Financed:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7">
+                        {!! Form::checkbox(
+                            'financed',
+                            1,
+                            isset($rentalEstimate)? $rentalEstimate->financed : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
 
-        <div class='form-group'>
-            {!! Form::label('total_loan_amount', 'Total Loan Amount:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'total_loan_amount',
-                    isset($rentalEstimate)? $rentalEstimate->total_loan_amount : null,
-                    ['class' => 'form-control', 'step' => '0.1']) !!}
-            </div>
-        </div>
+                {{--<div id="{{ 'financed-' . $rentalEstimate->id }}" style="display: none;">--}}
+                <div id="{{ 'financed-' . $rentalEstimate->id }}">
+                    <div class='form-group'>
+                        {!! Form::label('total_loan_amount', 'Total Loan Amount:', ['class' => 'col-md-5 control-label']) !!}
+                        <div class="col-md-7 input-group">
+                            <span class="input-group-addon">$</span>
+                            {!! Form::number(
+                                'total_loan_amount',
+                                isset($rentalEstimate)? $rentalEstimate->total_loan_amount : null,
+                                ['class' => 'form-control', 'step' => '0.1']) !!}
+                        </div>
+                    </div>
 
-        <div class='form-group'>
-            {!! Form::label('interest_rate', 'Interest Rate:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                {!! Form::number(
-                    'interest_rate',
-                    isset($rentalEstimate)? $rentalEstimate->interest_rate : null,
-                    ['class' => 'form-control', 'step' => '0.001']) !!}
-                <span class="input-group-addon">%</span>
-            </div>
-        </div>
+                    <div class='form-group'>
+                        {!! Form::label('interest_rate', 'Interest Rate:', ['class' => 'col-md-5 control-label']) !!}
+                        <div class="col-md-7 input-group">
+                            {!! Form::number(
+                                'interest_rate',
+                                isset($rentalEstimate)? $rentalEstimate->interest_rate : null,
+                                ['class' => 'form-control', 'step' => '0.001']) !!}
+                            <span class="input-group-addon">%</span>
+                        </div>
+                    </div>
 
-        <div class='form-group'>
-            {!! Form::label('term', 'Term:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                {!! Form::number(
-                    'term',
-                    isset($rentalEstimate)? $rentalEstimate->term : null,
-                    ['class' => 'form-control']) !!}
-                <span class="input-group-addon">
+                    <div class='form-group'>
+                        {!! Form::label('term', 'Term:', ['class' => 'col-md-5 control-label']) !!}
+                        <div class="col-md-7 input-group">
+                            {!! Form::number(
+                                'term',
+                                isset($rentalEstimate)? $rentalEstimate->term : null,
+                                ['class' => 'form-control']) !!}
+                            <span class="input-group-addon">
                     {!! Form::select(
                         'time_units',
                         [
@@ -104,11 +108,14 @@
                             'months' => 'months',
                         ]
                     ) !!}
-               </span>
+                       </span>
 
-            </div>
-        </div>
-    </div>
+                        </div>
+                    </div>
+                </div>
+            </div> <!-- col-md-4 -->
+        </div> <!-- row -->
+    </div> <!-- well -->
 
     <div class="well">
         <h4>Income</h4>
@@ -149,37 +156,37 @@
 
     <div class="well">
         <h4>Expenses</h4>
-        <div class='form-group'>
-            {!! Form::label('annual_taxes', 'Annual Taxes:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'annual_taxes',
-                    isset($rentalEstimate)? $rentalEstimate->annual_taxes : null,
-                    ['class' => 'form-control']) !!}
-            </div>
-        </div>
-
-        <div class='form-group'>
-            {!! Form::label('insurance', 'Insurance:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'insurance',
-                    isset($rentalEstimate)? $rentalEstimate->insurance : null,
-                    ['class' => 'form-control']) !!}
-            </div>
-        </div>
-
-        <div class='form-group'>
-            {!! Form::label('hoa', 'Home Owners Association:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'hoa',
-                    isset($rentalEstimate)? $rentalEstimate->hoa : null,
-                    ['class' => 'form-control']) !!}
-                <span class="input-group-addon">
+        <div class="row">
+            <div class="col-md-4">
+                <div class='form-group'>
+                    {!! Form::label('annual_taxes', 'Annual Taxes:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'annual_taxes',
+                            isset($rentalEstimate)? $rentalEstimate->annual_taxes : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+                <div class='form-group'>
+                    {!! Form::label('insurance', 'Insurance:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'insurance',
+                            isset($rentalEstimate)? $rentalEstimate->insurance : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+                <div class='form-group'>
+                    {!! Form::label('hoa', 'Home Owners Association:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'hoa',
+                            isset($rentalEstimate)? $rentalEstimate->hoa : null,
+                            ['class' => 'form-control']) !!}
+                        <span class="input-group-addon">
                     {!! Form::select(
                         'hoa_term',
                         [
@@ -188,64 +195,92 @@
                         ]
                     ) !!}
                </span>
+                    </div>
+                </div>
+            </div>
+            <div class ="col-md-8">
             </div>
         </div>
 
-        <div class='form-group'>
-            {!! Form::label('use_property_management', 'Use Property Management:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8">
-                {!! Form::checkbox(
-                    'use_property_management',
-                    1,
-                    isset($rentalEstimate)? $rentalEstimate->use_property_management : null,
-                    ['class' => 'form-control']) !!}
+
+        <div class="row">
+            <div class="col-md-4">
+                <div class='form-group'>
+                    {!! Form::label('use_property_management', 'Use Property Management:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-lg-8">
+                        {!! Form::checkbox(
+                            'use_property_management',
+                            1,
+                            isset($rentalEstimate)? $rentalEstimate->use_property_management : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+
+                <div class='form-group'>
+                    {!! Form::label('property_management_fee', 'Property Management Fee:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'property_management_fee',
+                            isset($rentalEstimate)? $rentalEstimate->property_management_fee : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
             </div>
         </div>
 
-        <div class='form-group'>
-            {!! Form::label('property_management_fee', 'Property Management Fee:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'property_management_fee',
-                    isset($rentalEstimate)? $rentalEstimate->property_management_fee : null,
-                    ['class' => 'form-control']) !!}
+        <div class="row">
+            <div class="col-md-4">
+                <div class='form-group'>
+                    {!! Form::label('capital_expenditures', 'Capital Expenditures:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'capital_expenditures',
+                            isset($rentalEstimate)? $rentalEstimate->capital_expenditures : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
             </div>
         </div>
 
-        <div class='form-group'>
-            {!! Form::label('capital_expenditures', 'Capital Expenditures:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'capital_expenditures',
-                    isset($rentalEstimate)? $rentalEstimate->capital_expenditures : null,
-                    ['class' => 'form-control']) !!}
+        <div class="row">
+            <div class="col-md-4">
+                <div class='form-group'>
+                    {!! Form::label('vacancy', 'Vacancy:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'vacancy',
+                            isset($rentalEstimate)? $rentalEstimate->vacancy : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
             </div>
         </div>
 
-        <div class='form-group'>
-            {!! Form::label('vacancy', 'Vacancy:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'vacancy',
-                    isset($rentalEstimate)? $rentalEstimate->vacancy : null,
-                    ['class' => 'form-control']) !!}
+        <div class="row">
+            <div class="col-md-4">
+                <div class='form-group'>
+                    {!! Form::label('monthly_repairs', 'Monthly Repairs:', ['class' => 'col-md-5 control-label']) !!}
+                    <div class="col-md-7 input-group">
+                        <span class="input-group-addon">$</span>
+                        {!! Form::number(
+                            'monthly_repairs',
+                            isset($rentalEstimate)? $rentalEstimate->monthly_repairs : null,
+                            ['class' => 'form-control']) !!}
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
             </div>
         </div>
-
-        <div class='form-group'>
-            {!! Form::label('monthly_repairs', 'Monthly Repairs:', ['class' => 'col-lg-2 control-label']) !!}
-            <div class="col-lg-8 input-group">
-                <span class="input-group-addon">$</span>
-                {!! Form::number(
-                    'monthly_repairs',
-                    isset($rentalEstimate)? $rentalEstimate->monthly_repairs : null,
-                    ['class' => 'form-control']) !!}
-            </div>
-        </div>
-    </div>
 
     <br/>
 </fieldset>
